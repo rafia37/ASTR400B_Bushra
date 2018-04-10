ebcaecb6329642ee0774a16f54384d15a9fc08af
rafia37 <rafia0037@gmail.com>
2018-04-10 14:27:13 -0700

    determined com positions of the galaxies

--- Research_Assignments/draft_code.py
+++ Research_Assignments/draft_code.py
@@ -17,3 +17,13 @@ M31_data = Read('../../M31_000.txt')[2]
 #Learn to calculate escape velocity of a galaxy
 #if some particles have velocities higher than the escape velocity of M31, they are unbound
 #Calculate the fraction of unbound particles (answers question 4)
+
+#creating center of ass objects using disk particles
+MWCOM  = CenterOfMass("../../MW_000.txt", 2)
+M31COM = CenterOfMass("../../M31_000.txt", 2)
+
+#Present day com positions of the two galaxies
+mw_xcom, mw_ycom, mw_zcom = MWCOM.COM_P(1)
+m31_xcom, m31_ycom, m31_zcom = M31COM.COM_P(1)
+
+#need to complete hw5 to procede
