298a3dd283862617226b2274943743e201c94743
rafia37 <rafia0037@gmail.com>
2018-04-16 22:53:19 -0700

    started looping over snapshots

--- HW6/Orbit.py
+++ HW6/Orbit.py
@@ -9,7 +9,18 @@ def OrbitCOM(self, galaxy, start, end, n):
     
     fileout = "Orbit_%s.txt" % galaxy
     size = int(end/n) + 1
-    Orbit = np.zeros(size)
+    Orbit = np.zeros([size, 7])
+    delta, VolDec = 0.5, 4
+    
+    for i in np.arange(start, end+1, n):
+        
+        #Creating filename for desired snapshot
+        ilbl = '000' + str(i)
+        ilbl = ilbl[-3:]  #making 3 digit numbers
+        filename = '%s_' % galaxy + ilbl + '.txt'
+        
+        COM = CenterOfMass(filename, 2)
+        
     
         
         
