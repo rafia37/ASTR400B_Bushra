3c9c9769b794d01d8816bc473cdf33962813332c
rafia37 <rafia0037@gmail.com>
2018-04-29 12:00:10 -0700

    Adding separation plots

--- HW6/Orbit.py
+++ HW6/Orbit.py
@@ -1,5 +1,9 @@
 import numpy as np
 import astropy.units as u
+from astropy.io import ascii
+from astropy.table import Table
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from ReadFile import Read
 from CenterOfMass import CenterOfMass
@@ -34,6 +38,32 @@ def OrbitCOM(galaxy, start, end, n):
     
     return fileout
 
-orb_mw  = OrbitCOM('MW', 0, 800, 5)
-orb_m31 = OrbitCOM('M31', 0, 800, 5)
-orb_m33 = OrbitCOM('M33', 0, 800, 5)
+def magnitude(x, y, z = 0):
+    return np.sqrt(x**2 + y**2 + z**2)
+
+#Uncomment the statements below to generate new orbit file
+#orb_mw  = OrbitCOM('MW', 0, 800, 5)
+#orb_m31 = OrbitCOM('M31', 0, 800, 5)
+#orb_m33 = OrbitCOM('M33', 0, 800, 5)
+
+#Reading in the orbit files
+mw_data = ascii.read('Orbit_MW.txt')
+m31_data = ascii.read('Orbit_M31.txt')
+m33_data = ascii.read('Orbit_M33.txt')
+
+mw_m31_sep = magnitude(mw_data['x'] - m31_data['x'], mw_data['y'] - m31_data['y'], mw_data['z'] - m31_data['z'])
+
+m31_m33_sep = magnitude(m31_data['x'] - m33_data['x'], m31_data['y'] - m33_data['y'], m31_data['z'] - m33_data['z'])
+
+plt.plot(mw_data['t'], mw_m31_sep)
+plt.xlabel('Time[Gyr]')
+plt.ylabel('Speration [kpc]')
+plt.title('Spatial separation between MW and M31')
+plt.savefig('MW_M31_Sep.png')
+plt.cla()
+
+plt.plot(m31_data['t'], m31_m33_sep)
+plt.xlabel('Time[Gyr]')
+plt.ylabel('Speration [kpc]')
+plt.title('Spatial separation between M31 and M33')
+plt.savefig('M31_M33_Sep.png')
