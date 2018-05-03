3b9c6b380f41a5d9439d32f6617eb0bfad9a60d1
rafia37 <rafia0037@gmail.com>
2018-05-02 17:38:07 -0700

    Starting Homework 7. Added all the necessary files and initialized the class

--- HW7/M33AnalyticOrbit.py
+++ HW7/M33AnalyticOrbit.py
@@ -1,7 +1,6 @@
 import numpy as np
 import astropy.units as u
-import sys, pdb
-sys.path.insert(0, '../HW2/') #Making python search the HW2 directory for ReadFile program
+import pdb
 from ReadFile import Read
 
 
